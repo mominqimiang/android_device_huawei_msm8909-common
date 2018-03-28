7c3d2e26b3438ed160c226ea0be88e25b6475137
Runner85sx <runner85sx@gmail.com>
2018-03-28 19:32:07 +0200

    Remove unused flags

--- board/00-qcom-platform-msm8909.mk
+++ board/00-qcom-platform-msm8909.mk
@@ -9,9 +9,6 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a7
 
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=softfp
-
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8909
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8909
