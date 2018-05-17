7035e8b3a17c141448f1354426186d269515f224
Runner85sx <runner85sx@yandex.ru>
2018-05-17 15:19:39 +0200

    msm8909-common: Cleanup & update props * Remove unused props * Update audio props for vendor move

--- product/perf.mk
+++ product/perf.mk
@@ -1,7 +1,3 @@
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sys.fw.dex2oat_thread_count=4
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.am.reschedule_service=true \
     ro.config.max_starting_bg=8
