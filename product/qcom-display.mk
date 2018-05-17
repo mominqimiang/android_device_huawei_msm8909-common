a16c1362d6854b368c696a28538950f62fcbe02d
Runner85sx <runner85sx@yandex.ru>
2018-05-17 15:28:43 +0200

    msm8909-common: enable debug.sf.enable_hwc_vds Enable debug.sf.enable_hwc_vds for virtual display to work

--- product/qcom-display.mk
+++ product/qcom-display.mk
@@ -18,6 +18,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.composition.type=dyn \
     debug.mdpcomp.idletime=600 \
+    debug.sf.enable_hwc_vds=1 \
     persist.hwc.mdpcomp.enable=true \
     persist.hwc.ptor.enable=true \
     debug.enable.sglscale=1
